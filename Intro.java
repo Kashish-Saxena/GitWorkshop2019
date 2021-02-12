0227dfab5d7cd21ec1e69908dacd9c934b15acc9
Kashish708 <s.kashish0123@gmail.com>
2021-02-11 21:41:01 -0500

    Added Javadoc Comments

--- Intro.java
+++ Intro.java
@@ -1,15 +1,33 @@
+/**
+ * This is an Introduction class for git testing.
+ * 
+ * @author Kashish Saxena
+ * @version 1.0
+ *
+ */
 public class Intro {
   
   private String name;
+  
+  /**
+   * Constructor of the Intro class
+   * 
+   * @param name Name of the person
+   */
   public Intro(String name)
   {
        this.name = name;
   }
   
+  /**
+   * Returns the name of the person
+   * @return The name of the person
+   */
   public String getName(){
     return name;
   }
-
+  
+  // Main function
   public static void main( String args[] )
   {
       Intro i = new Intro("Kashish");
